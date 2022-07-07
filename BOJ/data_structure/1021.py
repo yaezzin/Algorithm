b88bcb8cae40e7c3a6a84961a34acf20b1e75a3f
yejin <yaezzin@gmail.com>
2022-07-07 20:38:55 +0900

    1021 회전하는 큐

---
+++ BOJ/data_structure/1021.py
@@ -0,0 +1,26 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+pick = list(map(int, input().split()))
+dq = deque([i for i in range(1, n+1)])
+count = 0
+
+for i in pick:
+    while True:
+        if dq[0] == i:
+            dq.popleft()
+            break
+        else:
+            if dq.index(i) < len(dq)//2:
+                while dq[0] != i:
+                    dq.append(dq.popleft())
+                    count += 1
+            else:
+                while dq[0] != i:
+                    dq.appendleft(dq.pop())
+                    count += 1
+
+    print(count)
+                     
