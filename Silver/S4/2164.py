aad8e3e6232256072cbf68a888e40990fe71c302
yaezzin <yaezzin@gmail.com>
2023-08-30 18:57:14 +0900

    2164 카드 2

---
+++ Silver/S4/2164.py
@@ -0,0 +1,15 @@
+# 2164 카드 2
+
+from collections import deque
+
+n = int(input())
+
+queue = deque()
+for i in range(1, n+1):
+    queue.append(i)
+
+while len(queue) >= 2:
+    queue.popleft()
+    queue.append(queue.popleft())
+
+print(queue[0])
