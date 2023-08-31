401092253606184f882a49ef9e1b7f388d3ab9a9
yaezzin <yaezzin@gmail.com>
2023-08-31 23:47:48 +0900

    1978 소수 찾기 - 제곱근

--- Bronze/B2/1978.py
+++ Bronze/B2/1978.py
@@ -1,10 +1,13 @@
 ## 1978 소수 찾기
 
+import math
+
+
 def isPrime(n):
     if n < 2:
         return False
     
-    for i in range(2, n):
+    for i in range(2, int(math.sqrt(n) + 1)):
         if n % i == 0:
             return False      
     return True
