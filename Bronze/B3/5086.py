2f0dd2d1daa269fe9c87543aac3502d2e6ca78b8
yaezzin <yaezzin@gmail.com>
2023-09-22 23:21:08 +0900

    5086 배수와 약수

---
+++ Bronze/B3/5086.py
@@ -0,0 +1,16 @@
+
+
+while True:
+    a, b = map(int, input().split())
+
+    if a == 0 and b == 0:
+        break
+
+    if b % a == 0:
+        print("factor")
+    
+    elif a > b and a % b == 0:
+        print("multiple")
+
+    else:
+        print("neither")
