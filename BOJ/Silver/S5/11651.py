292ff28d2ae4f35ba7d267c01c9e32dbb3eba679
yaezzin <yaezzin@gmail.com>
2023-10-27 23:57:15 +0900

    delete files

--- BOJ/Silver/S5/11651.py
+++
@@ -1,5 +0,0 @@
-n = int(input())
-list = [input().split() for _ in range(n)]
-list.sort(key=lambda x : (int(x[1]), int(x[0])))
-for i in list:
-    print(i[0], i[1])
--- BOJ/Silver/S5/1427.py
+++
@@ -1,3 +0,0 @@
-list = list(input())
-list.sort(reverse=True)
-print(''.join(list))
