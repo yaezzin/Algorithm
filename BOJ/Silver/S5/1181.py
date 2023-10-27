11a12c3b8ab1015f9090c74156e796f0b4a11dce
yaezzin <yaezzin@gmail.com>
2023-10-27 16:10:09 +0900

    1181 단어 정렬

---
+++ BOJ/Silver/S5/1181.py
@@ -0,0 +1,14 @@
+n = int(input())
+l = [input() for _ in range(n)]
+
+# 1. 중복 제거를 위해 set으로 감싼 후 다시 list로 감싸기
+list = list(set(l))
+
+# 2. 사전순 정렬
+list.sort()
+
+# 3. 길이순 정렬
+list.sort(key=len)
+
+for i in list:
+    print(i) 
