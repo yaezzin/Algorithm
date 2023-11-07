a2d37cebfe083a3e6e58984aed69967d1d99184c
yaezzin <yaezzin@gmail.com>
2023-11-07 18:33:51 +0900

    11286 절대값힙

---
+++ BOJ/Silver/S1/11286.py
@@ -0,0 +1,16 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+heap = []
+for i in range(n):
+    num = int(input())
+    
+    if num == 0:
+        if len(heap) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(heap)[1])
+    else:
+        heapq.heappush(heap, (abs(num), num))
