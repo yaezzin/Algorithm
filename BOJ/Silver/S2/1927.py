3410d6fe4d61ef629d5acd99b943198695d563b7
yaezzin <yaezzin@gmail.com>
2023-11-07 18:10:44 +0900

    1927 최소 힙

---
+++ BOJ/Silver/S2/1927.py
@@ -0,0 +1,16 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+heap = []
+for i in range(n):
+    # 0 : 제거 후 출력
+    num = int(input())
+    if num == 0:
+        if len(heap) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(heap))
+    else:
+        heapq.heappush(heap, num)
