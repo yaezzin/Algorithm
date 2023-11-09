fbb3d88bc1b4bbeeef76f0747d52c1b53ed1ae22
yaezzin <yaezzin@gmail.com>
2023-11-10 01:27:38 +0900

    10866 덱

---
+++ BOJ/Silver/S4/10866.py
@@ -0,0 +1,49 @@
+from collections import deque
+import sys
+
+n = int(sys.stdin.readline())
+queue = deque()
+
+for i in range(n):
+    command = sys.stdin.readline().split()
+
+    if command[0] == 'push_front':
+        queue.appendleft(command[1])
+    
+    elif command[0] == 'push_back':
+        queue.append(command[1])
+
+    elif command[0] == 'pop_front':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue.popleft())
+    
+    elif command[0] == 'pop_back':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue.pop())
+            
+    elif command[0] == 'size':
+        print(len(queue))
+    
+    elif command[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    
+    elif command[0] == 'front':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[0])
+    
+    elif command[0] == 'back':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[-1])
+    
+       
